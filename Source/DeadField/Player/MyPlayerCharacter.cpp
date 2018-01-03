d140bf9c7a40d8f81b6de408b7247879e9a9c725
kiteg <kiteg21@gmail.com>
2018-01-03 09:45:55 +0300

    Initial commit

---
+++ Source/DeadField/Player/MyPlayerCharacter.cpp
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Classes/Kismet/KismetMathLibrary.h"
+
+
+
+#include "MyPlayerCharacter.h"
+
+
+// Sets default values
+AMyPlayerCharacter::AMyPlayerCharacter(const FObjectInitializer &ObjectInitializer):Super(ObjectInitializer)
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	cameraSpring = ObjectInitializer.CreateDefaultSubobject<USpringArmComponent>(this, "CameraSpring");
+	cameraSpring->SetupAttachment(RootComponent);
+	cameraSpring->RelativeRotation = FRotator(-90.f, 0.f, 0.f);
+
+	//cameraSpring->bEnableCameraLag = true;//Включить плавное движение камеры
+
+	//UCameraComponent *cameraComp;
+	cameraComp = ObjectInitializer.CreateDefaultSubobject<UCameraComponent>(this, "CameraComponent");
+	cameraComp->SetupAttachment(cameraSpring, USpringArmComponent::SocketName);
+
+	GetMesh()->AddLocalOffset(FVector(0, 0, -90));
+	GetMesh()->AddLocalRotation(FRotator(0, -90, 0));
+}
+
+// Called when the game starts or when spawned
+void AMyPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	pc = GetWorld()->GetFirstPlayerController();
+
+	cameraSpring->bUsePawnControlRotation;
+	cameraSpring->bInheritYaw = false;
+	cameraSpring->bInheritPitch = false;
+	cameraSpring->bInheritRoll = false;
+	cameraSpring->TargetArmLength = CameraDistance;//Высота обзора
+	
+	pc->InputYawScale = TurnSpeed;//Скорость поворота. Чем выше скорость, тем меньше плавность
+
+}
+
+// Called every frame
+void AMyPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis("MoveForward", this, &AMyPlayerCharacter::moveForward);
+	PlayerInputComponent->BindAxis("Strafe", this, &AMyPlayerCharacter::strafe);
+	PlayerInputComponent->BindAxis("Turn", this, &AMyPlayerCharacter::turn);
+}
+
+void AMyPlayerCharacter::moveForward(float step)
+{
+	//const FVector directoin = FRotationMatrix(FRotator(0, GetControlRotation().Yaw, 0)).GetUnitAxis(EAxis::X);//движение в направлении камеры
+	const FVector direction = FVector(1, 0, 0);//Независимо от направления камеры
+	AddMovementInput(direction, step);
+}
+
+void AMyPlayerCharacter::strafe(float step)
+{
+	//const FVector dir = FRotationMatrix(FRotator(0, GetControlRotation().Yaw, 0)).GetUnitAxis(EAxis::Y);//движение в направлении камеры
+	const FVector direction = FVector(0, 1, 0);//движение независимо от камеры
+	AddMovementInput(direction, step);
+}
+
+void AMyPlayerCharacter::turn(float step)
+{
+	FHitResult hit;
+	pc->GetHitResultUnderCursor(ECollisionChannel::ECC_MAX, false, hit);
+	//FRotator rot = (GetActorLocation() - hit.ImpactPoint).Rotation;
+	
+	FVector dir = hit.ImpactPoint - GetActorLocation();
+	AddControllerYawInput(FMath::Atan2(GetActorForwardVector().X*dir.Y - dir.X*GetActorForwardVector().Y, GetActorForwardVector().X*dir.X + GetActorForwardVector().Y*dir.Y));
+}
+
---
+++ Source/DeadField/Player/MyPlayerCharacter.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+#include "GameFramework/Character.h"
+#include "MyPlayerCharacter.generated.h"
+
+UCLASS()
+class DEADFIELD_API AMyPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMyPlayerCharacter(const FObjectInitializer &ObjectInitializer);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	USpringArmComponent *cameraSpring;
+	UCameraComponent *cameraComp;
+	USceneComponent *sceneComp;
+	APlayerController *pc;
+
+	void moveForward(float step);
+	void strafe(float step);
+	void turn(float step);
+
+	UPROPERTY(Category = "PlayerSettings", EditDefaultsOnly)//Скорость поворота. Чем выше скорость, тем меньше плавность
+		float TurnSpeed = 5.f;
+	UPROPERTY(Category = "PlayerSettings", EditDefaultsOnly)//Высота обзора
+		float CameraDistance = 800.f;
+	
+};
