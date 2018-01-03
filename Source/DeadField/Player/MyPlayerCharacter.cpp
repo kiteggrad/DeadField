0ca54a772bad02080f9203bc9bc7979c6cbe1756
kiteg <kiteg21@gmail.com>
2018-01-03 10:00:20 +0300

    FixedCharacter

--- Source/DeadField/Player/MyPlayerCharacter.cpp
+++ Source/DeadField/Player/MyPlayerCharacter.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Classes/Kismet/KismetMathLibrary.h"
 
 
 
 #include "MyPlayerCharacter.h"
+#include "Classes/Kismet/KismetMathLibrary.h"
 
 
 // Sets default values
